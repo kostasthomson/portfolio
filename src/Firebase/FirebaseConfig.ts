adf0e8910bd53d1b46543abc0196dfde8a4e7678
Kostas Thomson <kostasthomson12@gmail.com>
2025-06-13 22:18:59 +0300

    async 2

--- src/Firebase/FirebaseConfig.ts
+++ src/Firebase/FirebaseConfig.ts
@@ -1,24 +1,26 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
-async function fetchServerlessFunction() {
-  const { event, context, ...firebase }: any = await fetch(
-    'https://reliable-belekoy-ddd42b.netlify.app/.netlify/functions/main',
-    { method: 'POST' }
-  );
-  return firebase;
-}
-const firebase: any = await fetchServerlessFunction();
-const firebaseConfig = {
-  apiKey: firebase.FIREBASE_ApiKey,
-  authDomain: firebase.FIREBASE_AuthDomain,
-  databaseURL: firebase.FIREBASE_DatabaseURL,
-  projectId: firebase.FIREBASE_ProjectId,
-  storageBucket: firebase.FIREBASE_StorageBucket,
-  messagingSenderId: firebase.FIREBASE_MessagingSenderId,
-  appId: firebase.FIREBASE_AppId,
-  measurementId: firebase.FIREBASE_MeasurementId,
-};
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+
+let database: any;
+
+fetch('https://reliable-belekoy-ddd42b.netlify.app/.netlify/functions/main', {
+  method: 'POST',
+})
+  .then((response) => {
+    const { event, context, ...firebase }: any = response.json();
+    const firebaseConfig = {
+      apiKey: firebase.FIREBASE_ApiKey,
+      authDomain: firebase.FIREBASE_AuthDomain,
+      databaseURL: firebase.FIREBASE_DatabaseURL,
+      projectId: firebase.FIREBASE_ProjectId,
+      storageBucket: firebase.FIREBASE_StorageBucket,
+      messagingSenderId: firebase.FIREBASE_MessagingSenderId,
+      appId: firebase.FIREBASE_AppId,
+      measurementId: firebase.FIREBASE_MeasurementId,
+    };
+    const app = initializeApp(firebaseConfig);
+    database = getDatabase(app);
+  })
+  .catch(console.error);
 
 export { database };
