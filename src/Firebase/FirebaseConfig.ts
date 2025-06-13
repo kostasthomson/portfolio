e9ae2ff1814d79e9068aca54c60f43d7d49d145f
Kostas Thomson <kostasthomson12@gmail.com>
2025-06-13 21:49:15 +0300

    firebase config

--- src/Firebase/FirebaseConfig.ts
+++ src/Firebase/FirebaseConfig.ts
@@ -1,15 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
+const {event, context, ...firebase} = await fetch('https://reliable-belekoy-ddd42b.netlify.app/.netlify/functions/main');
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_ApiKey,
-  authDomain: import.meta.env.VITE_FIREBASE_AuthDomain,
-  databaseURL: import.meta.env.VITE_FIREBASE_DatabaseURL,
-  projectId: import.meta.env.VITE_FIREBASE_ProjectId,
-  storageBucket: import.meta.env.VITE_FIREBASE_StorageBucket,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MessagingSenderId,
-  appId: import.meta.env.VITE_FIREBASE_AppId,
-  measurementId: import.meta.env.VITE_FIREBASE_MeasurementId,
+  apiKey: firebase.FIREBASE_ApiKey,
+  authDomain: firebase.FIREBASE_AuthDomain,
+  databaseURL: firebase.FIREBASE_DatabaseURL,
+  projectId: firebase.FIREBASE_ProjectId,
+  storageBucket: firebase.FIREBASE_StorageBucket,
+  messagingSenderId: firebase.FIREBASE_MessagingSenderId,
+  appId: firebase.FIREBASE_AppId,
+  measurementId: firebase.FIREBASE_MeasurementId,
 };
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
